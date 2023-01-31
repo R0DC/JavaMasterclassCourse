b8f20547c8cf17de06c79cf3aee22ecb2ada6095
Rodion Calistru <rocalistru@gmail.com>
2023-01-31 16:02:08 -0500

    Encapsulation challenge - printer

---
+++ PrinterChallenge/src/Printer.java
@@ -0,0 +1,25 @@
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.tonerLevel = (tonerLevel >= 0 && tonerLevel <= 100) ? tonerLevel : -1;
+        this.duplex = duplex;
+        this.pagesPrinted = 0;
+    }
+
+    public int addToner(int tonerAmount) {
+        return (tonerLevel + tonerAmount >= 0 && tonerLevel + tonerAmount <= 100) ? tonerLevel + tonerAmount : -1;
+    }
+
+    public int printPages(int pages){
+        int jobPages = (duplex) ? (pages / 2) + (pages % 2) : pages;
+        pagesPrinted += jobPages;
+        return jobPages;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+}
