1d0c60be6b481b1eb846cd6ad166134623ff4f75
Rodion Calistru <rocalistru@gmail.com>
2023-02-01 16:18:55 -0500

    Polymorphism lesson - Movie category example

---
+++ Polymorphism/src/NextMain.java
@@ -0,0 +1,32 @@
+import javax.management.AttributeValueExp;
+import java.io.ObjectStreamException;
+
+public class NextMain {
+    public static void main(String[] args) {
+        Movie movie = Movie.getMovie("A", "Jaws");
+        movie.watchMovie();
+
+        Adventure jaws = (Adventure) Movie.getMovie("A", "Jaws");
+        jaws.watchMovie();
+
+        Object comedy = Movie.getMovie("c", "Airplane");
+        Comedy comedyMovie = (Comedy) comedy;
+        comedyMovie.watchMovie();
+
+        var airplane = Movie.getMovie("C","Airplane");
+        airplane.watchMovie();
+
+        var plane = new Comedy("Airplane");
+        plane.watchComedy();
+
+        Object unknownObject = Movie.getMovie("A","Airplane");
+        if(unknownObject.getClass().getSimpleName() == "Comedy"){
+            Comedy c = (Comedy) unknownObject;
+            c.watchComedy();
+        } else if (unknownObject instanceof Adventure){
+            ((Adventure) unknownObject).watchAdventure();
+        } else if (unknownObject instanceof ScienceFiction syfy){
+            syfy.watchSciFi();
+        }
+    }
+}
